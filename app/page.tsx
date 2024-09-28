63df305a95774352ee62f00e3f7838a42bfe4420
Isaac <isaacspiegel@gmail.com>
2024-09-27 19:06:32 -0500

    Update links

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function IndexPage() {
       </div>
       <div className="flex gap-4">
         <Link
-          href={siteConfig.links.docs}
+          href={'/playground'}
           target="_blank"
           rel="noreferrer"
           className={buttonVariants()}

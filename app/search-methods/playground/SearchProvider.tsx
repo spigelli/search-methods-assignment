766d0c7ee182f84099b3312b0bfd7c3fbbac687c
Isaac <isaacspiegel@gmail.com>
2024-10-03 12:59:33 -0500

    Edit search provider

--- app/search-methods/playground/SearchProvider.tsx
+++ app/search-methods/playground/SearchProvider.tsx
@@ -1,34 +1,50 @@
-import { createContext, use, useContext } from "react";
+import { createContext, Dispatch, SetStateAction, use, useContext, useState } from "react";
 import { SearchMethodId } from "./util";
 
 type SearchingState = {
 }
 
-type SearchConfig = {
-  startTown: string | undefined;
-  endTown: string | undefined;
-  searchMethod: SearchMethodId | undefined;
-} & (
-  | ( { isSearching: true } & SearchingState )
-  | ( { isSearching: false } )
-);
 
-const SearchContext = createContext<SearchConfig>({
-  startTown: undefined,
-  endTown: undefined,
-  searchMethod: undefined,
-  isSearching: false,
-});
+const SearchContext = createContext<{
+  startTown: string | undefined;
+  setStartTown: Dispatch<SetStateAction<string | undefined>>;
+  endTown: string | undefined;
+  setEndTown: Dispatch<SetStateAction<string | undefined>>;
+  searchMethod: SearchMethodId | undefined;
+  setSearchMethod: Dispatch<SetStateAction<SearchMethodId | undefined>>;
+  isSearching: boolean;
+  setIsSearching: Dispatch<SetStateAction<boolean>>;
+} | undefined>(undefined);
 
 export function SearchProvider({ children }: { children: React.ReactNode }) {
+  const [startTown, setStartTown] = useState<string | undefined>(undefined);
+  const [endTown, setEndTown] = useState<string | undefined>(undefined);
+  const [searchMethod, setSearchMethod] = useState<SearchMethodId | undefined>(undefined);
+  const [isSearching, setIsSearching] = useState(false);
+
+  const value = {
+    startTown,
+    setStartTown,
+    endTown,
+    setEndTown,
+    searchMethod,
+    setSearchMethod,
+    isSearching,
+    setIsSearching,
+  };
+
   return (
-    <SearchContext.Provider value={{ startTown: undefined, endTown: undefined, searchMethod: undefined, isSearching: false }}>
+    <SearchContext.Provider value={value}>
       {children}
     </SearchContext.Provider>
   );
 }
 
 export function useSearch() {
-  return useContext(SearchContext);
+  const context = useContext(SearchContext);
+  if (context === undefined) {
+    throw new Error('useSearch must be used within a SearchProvider');
+  }
+  return context;
 }
 
